0cf85324134eb00540407e6631ce5719ffd26b62
Tran Hung <tran.hung0713@gmail.com>
2021-03-22 21:50:17 -0500

    updated

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // Routes //
 const userRoutes = require("./routes/user");
