eeeebe92dc4688d38945cf87b4659f2bfa4651b4
Tran Hung <tran.hung0713@gmail.com>
2021-03-22 23:11:52 -0500

    testing

--- app.js
+++ app.js
@@ -7,21 +7,17 @@ const http = require("http");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server, {
-  cors: {
-    origin: "https://powerful-citadel-38170.herokuapp.com/",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Authorization"],
-  },
-});
+// const io = socketio(server, {
+//   cors: {
+//     origin: "https://powerful-citadel-38170.herokuapp.com/",
+//     methods: ["GET", "POST"],
+//     allowedHeaders: ["Authorization"],
+//   },
+// });
 
-// app.use(express.json({ limit: "50mb" }));
-// app.use(express.urlencoded({ limit: "50mb", extended: true }));
-// app.use(cors());
-
-app.use(express.urlencoded());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
-app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
 
@@ -32,36 +28,36 @@ const chatRoomsRoutes = require("./routes/chatRooms");
 app.use("/user", userRoutes);
 app.use("/chat-room", chatRoomsRoutes);
 
-io.on("connection", (socket) => {
-  // console.log("We have a new connection");
-  socket.on("joinRoom", (roomId) => {
-    socket.join(roomId);
-    // console.log(`new user has joined ${roomId}`);
-  });
-  socket.on("leaveRoom", (roomId) => {
-    socket.leave(roomId);
-    // console.log(`new user has joined ${roomId}`);
-  });
+// io.on("connection", (socket) => {
+//   // console.log("We have a new connection");
+//   socket.on("joinRoom", (roomId) => {
+//     socket.join(roomId);
+//     // console.log(`new user has joined ${roomId}`);
+//   });
+//   socket.on("leaveRoom", (roomId) => {
+//     socket.leave(roomId);
+//     // console.log(`new user has joined ${roomId}`);
+//   });
 
-  socket.on("sendMessage", (messageDetail, callback) => {
-    io.in(messageDetail.roomId).emit("message", {
-      user: messageDetail.userName,
-      text: messageDetail.message,
-    });
-    callback();
-  });
+//   socket.on("sendMessage", (messageDetail, callback) => {
+//     io.in(messageDetail.roomId).emit("message", {
+//       user: messageDetail.userName,
+//       text: messageDetail.message,
+//     });
+//     callback();
+//   });
 
-  socket.on("disconnect", () => {
-    // console.log("User has left!!");
-  });
-});
+//   socket.on("disconnect", () => {
+//     // console.log("User has left!!");
+//   });
+// });
 
 app.get("/", (req, res) => {
   res.json({ Hello: "hello" });
 });
 
-server.listen(PORT, () => {
-  console.log("server is runing");
+app.listen(PORT, () => {
+  console.log("the sercer is running!");
 });
 // const express = require("express");
 // const app = express();
