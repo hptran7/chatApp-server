eba7904392a0d04ef095db44f0931f50558047d1
Tran Hung <tran.hung0713@gmail.com>
2021-03-22 22:15:57 -0500

    updated

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ require("dotenv").config();
 
 const app = express();
 const server = http.createServer(app);
-// const io = socketio(server, {
-//   cors: {
-//     origin: "https://powerful-citadel-38170.herokuapp.com/",
-//     methods: ["GET", "POST"],
-//     allowedHeaders: ["Authorization"],
-//   },
-// });
+const io = socketio(server, {
+  cors: {
+    origin: "https://powerful-citadel-38170.herokuapp.com/",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Authorization"],
+  },
+});
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Routes //
 const userRoutes = require("./routes/user");
@@ -27,29 +27,29 @@ const chatRoomsRoutes = require("./routes/chatRooms");
 app.use("/user", userRoutes);
 app.use("/chat-room", chatRoomsRoutes);
 
-// io.on("connection", (socket) => {
-//   // console.log("We have a new connection");
-//   socket.on("joinRoom", (roomId) => {
-//     socket.join(roomId);
-//     // console.log(`new user has joined ${roomId}`);
-//   });
-//   socket.on("leaveRoom", (roomId) => {
-//     socket.leave(roomId);
-//     // console.log(`new user has joined ${roomId}`);
-//   });
+io.on("connection", (socket) => {
+  // console.log("We have a new connection");
+  socket.on("joinRoom", (roomId) => {
+    socket.join(roomId);
+    // console.log(`new user has joined ${roomId}`);
+  });
+  socket.on("leaveRoom", (roomId) => {
+    socket.leave(roomId);
+    // console.log(`new user has joined ${roomId}`);
+  });
 
-//   socket.on("sendMessage", (messageDetail, callback) => {
-//     io.in(messageDetail.roomId).emit("message", {
-//       user: messageDetail.userName,
-//       text: messageDetail.message,
-//     });
-//     callback();
-//   });
+  socket.on("sendMessage", (messageDetail, callback) => {
+    io.in(messageDetail.roomId).emit("message", {
+      user: messageDetail.userName,
+      text: messageDetail.message,
+    });
+    callback();
+  });
 
-//   socket.on("disconnect", () => {
-//     // console.log("User has left!!");
-//   });
-// });
+  socket.on("disconnect", () => {
+    // console.log("User has left!!");
+  });
+});
 
 app.get("/", (req, res) => {
   res.json({ Hello: "hello" });
