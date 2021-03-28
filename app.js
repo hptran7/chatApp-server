8300919e561abf43c2e03f714221eae09d546aa7
Tran Hung <tran.hung0713@gmail.com>
2021-03-28 16:53:43 -0500

    updated

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: "https://frozen-sierra-45378.herokuapp.com",
+    origin: "https://http://chat-dc.surge.sh",
     methods: ["GET", "POST"],
     allowedHeaders: ["Authorization"],
   },
